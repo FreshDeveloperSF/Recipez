7cf6886a958bcc54679269feb190465b062b31e6
Jason Leung <jasonleung@jasons-mbp-3.att.net>
2016-06-21 22:06:48 -0700

    Initial Commit

---
+++ Recipez - CoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipez - CoreData
+//
+//  Created by Jason Leung on 6/21/16.
+//  Copyright © 2016 Jason Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
